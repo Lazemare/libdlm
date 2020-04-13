5dd65307a806774738b04891c175bb86d1584971
Lazemare <zjt033@gmail.com>
2020-04-13 21:14:41 +0800

    Change output

--- test/main.c
+++ test/main.c
@@ -3,52 +3,69 @@
 #include <string.h>
 #include "delimitedfiles.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	FILE *inp;
 	FILE *out;
-	inp = fopen("txt","r");
+	inp = fopen(argv[1],"r");
 	out = fopen("out","w");
 	int i,j;
+	int in,put;
 	int flag;
 	char list[MAXLINES][MAXWORDS][MAXLETTERS];
 
 	memset(list, 0, sizeof list);
-	readdlm(inp,list,' ',0,0,'#');
+	in = readdlm(inp,list,' ',0,0,'#');
 	printf("quotes = 0; comments = 0; comment_char='#':\n");
 	fprintf(out,"quotes = 0; comments = 0; comment_char='#':\n");
 	writedlm(stdout,list,' ');
-	writedlm(out,list,' ');
+	put = writedlm(out,list,' ');
+	fprintf(out,"%s %d\n","Total read-in words:",in);
+	fprintf(out,"%s %d\n","Total write-out words:",put);
+	printf("%s %d\n","Total read-in words:",in);
+	printf("%s %d\n","Total write-out words:",put);
 	fprintf(out,"\n");
 	printf("\n");
 
 	rewind(inp);
 	memset(list, 0, sizeof list);
-	readdlm(inp,list,' ',1,0,'#');
+	in = readdlm(inp,list,' ',1,0,'#');
 	printf("quotes = 1; comments = 0; comment_char='#':\n");
 	fprintf(out,"quotes = 1; comments = 0; comment_char='#':\n");
 	writedlm(stdout,list,' ');
-	writedlm(out,list,' ');
+	put = writedlm(out,list,' ');
+	fprintf(out,"%s %d\n","Total read-in words:",in);
+	fprintf(out,"%s %d\n","Total write-out words:",put);
+	printf("%s %d\n","Total read-in words:",in);
+	printf("%s %d\n","Total write-out words:",put);
 	fprintf(out,"\n");
 	printf("\n");
 
 	rewind(inp);
 	memset(list, 0, sizeof list);
-	readdlm(inp,list,' ',1,1,'#');
+	in = readdlm(inp,list,' ',1,1,'#');
 	printf("quotes = 1; comments = 1:\n");
 	fprintf(out,"quotes = 1; comments = 1:\n");
 	writedlm(stdout,list,' ');
-	writedlm(out,list,' ');
+	put = writedlm(out,list,' ');
+	fprintf(out,"%s %d\n","Total read-in words:",in);
+	fprintf(out,"%s %d\n","Total write-out words:",put);
+	printf("%s %d\n","Total read-in words:",in);
+	printf("%s %d\n","Total write-out words:",put);
 	fprintf(out,"\n");
 	printf("\n");
 
 	rewind(inp);
 	memset(list, 0, sizeof list);
-	readdlm(inp,list,' ',1,1,'#');
+	in = readdlm(inp,list,' ',1,1,'#');
 	printf("quotes = 1; comments = 1; output delim = '\\t':\n");
 	fprintf(out,"quotes = 1; comments = 1; output delim = '\\t':\n");
 	writedlm(stdout,list,'\t');
-	writedlm(out,list,'\t');
+	put = writedlm(out,list,'\t');
+	fprintf(out,"%s %d\n","Total read-in words:",in);
+	fprintf(out,"%s %d\n","Total write-out words:",put);
+	printf("%s %d\n","Total read-in words:",in);
+	printf("%s %d\n","Total write-out words:",put);
 	fprintf(out,"\n");
 	printf("\n");
 
