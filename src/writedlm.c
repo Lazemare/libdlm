8b283f1be58ad857641335aaff0d1527e325ee2a
Lazemare <zjt033@gmail.com>
2020-04-28 22:19:16 +0800

    Added struct

--- src/writedlm.c
+++ src/writedlm.c
@@ -4,23 +4,23 @@
 #include "delimitedfiles.h"
 
 /* Write delimited file. */
-int writedlm(FILE *fp, char list[MAXLINES][MAXWORDS][MAXLETTERS], char delim)
+int writedlm(FILE *fp, struct DLM *dlm, char delim)
 {
 	int i,j;
 	int flag = 0;
 	int count = 0;
 
-	for (i=0; i < MAXLINES; i++) {
+	for (i=0; i < (*dlm).linenum; i++) {
 		flag = 0;
 		for (j=0; j < MAXWORDS; j++) {
-			if (strcmp(list[i][j],"\0") > 0) {
-				fprintf(fp,"%s%c",list[i][j],delim);
+			if (strcmp((*dlm).list[i][j],"\0") > 0) {
+				fprintf(fp,"%s%c",(*dlm).list[i][j],delim);
 				flag = 1;
 				count++;
 			}
 		}
 		if (flag == 1) {
-			fprintf(fp,"\n");
+			fprintf(fp,"\n\n");
 		}
 	}
 	return count;
