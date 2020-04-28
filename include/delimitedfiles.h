10d620ee0bbfbb73edc90a639b04e841d1aab535
Lazemare <zjt033@gmail.com>
2020-04-28 22:17:06 +0800

    Added struct

--- include/delimitedfiles.h
+++ include/delimitedfiles.h
@@ -1,12 +1,23 @@
 /* Max line number. */
 #define MAXLINES 200
 /* Max word number per line. */
-#define MAXWORDS 100
+#define MAXWORDS 50
 /* Max letter number per word. */
-#define MAXLETTERS 100
+#define MAXLETTERS 50
 /* End of line */
 #define EOL '\n'
 
-int readdlm(FILE *fp, char list[MAXLINES][MAXWORDS][MAXLETTERS], \
-	char delim,int quotes, int comments, char comment_char);
-int writedlm(FILE *fp, char list[MAXLINES][MAXWORDS][MAXLETTERS], char delim);
+struct DLM {
+	/* Line number */
+	int linenum;
+	/* Word number */
+	int wordnum;
+	/* Main array */
+	char ***list;
+};
+
+void init_dlm(struct DLM *dlm);
+void free_dlm(struct DLM *dlm);
+void readdlm(FILE *fp, struct DLM *dlm, char delim, int quotes, \
+	int comments, char comment_char);
+int writedlm(FILE *fp, struct DLM *dlm, char delim);
