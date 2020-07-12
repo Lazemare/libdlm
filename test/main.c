8e164f0119c72bd55a3c05e787e17c8c8f91ab6a
Lazemare <zjt033@gmail.com>
2020-07-13 00:58:31 +0800

    Update main.c

--- test/main.c
+++ test/main.c
@@ -3,68 +3,84 @@
 #include <string.h>
 #include "delimitedfiles.h"
 
-/* Make a small test. */
+#define DLM_COUNT(dlm,count) do { \
+	count = 0; \
+	for (int i = 0; i < dlm.linenum; i++) { \
+		count += dlm.wordnum[i]; \
+	} \
+} while (0)
+
 int main(int argc, char *argv[])
 {
-	FILE *inp;
-	FILE *out;
+	FILE *inp = NULL;
+	FILE *out = NULL;
 	inp = fopen(argv[1],"r");
 	out = fopen("out","w");
-	int i,j,writenum;
-	int flag;
-	struct DLM dlm;
-	init_dlm(&dlm);
+	int readnum = 0;
+	int writenum = 0;
+	dlm_t dlm;
 
 	readdlm(inp,&dlm,' ',1,1,'#');
+	DLM_COUNT(dlm,readnum);
 	printf("---- quotes = 1; comments = 1; comment_char='#'; ----\n\n");
 	fprintf(out,"---- quotes = 1; comments = 1; comment_char='#'; ----\n\n");
 	writedlm(stdout,&dlm,' ');
 	writenum = writedlm(out,&dlm,' ');
-	fprintf(out,"%s %d\n","Total read-in words:",dlm.wordnum);
+	fprintf(out,"%s %d\n","Total read-in words:",readnum);
 	fprintf(out,"%s %d\n","Total write-out words:",writenum);
-	printf("%s %d\n","Total read-in words:",dlm.wordnum);
+	printf("%s %d\n","Total read-in words:",readnum);
 	printf("%s %d\n","Total write-out words:",writenum);
 	fprintf(out,"-----------------------------------------------------\n\n");
 	printf("-----------------------------------------------------\n\n");
+	fflush(stdout);
+	free_dlm(&dlm);
 
 	rewind(inp);
 	readdlm(inp,&dlm,' ',0,1,'#');
+	DLM_COUNT(dlm,readnum);
 	printf("---- quotes = 0; comments = 1; comment_char='#'; ----\n\n");
 	fprintf(out,"---- quotes = 0; comments = 1; comment_char='#'; ----\n\n");
 	writedlm(stdout,&dlm,' ');
 	writenum = writedlm(out,&dlm,' ');
-	fprintf(out,"%s %d\n","Total read-in words:",dlm.wordnum);
+	fprintf(out,"%s %d\n","Total read-in words:",readnum);
 	fprintf(out,"%s %d\n","Total write-out words:",writenum);
-	printf("%s %d\n","Total read-in words:",dlm.wordnum);
+	printf("%s %d\n","Total read-in words:",readnum);
 	printf("%s %d\n","Total write-out words:",writenum);
 	fprintf(out,"-----------------------------------------------------\n\n");
 	printf("-----------------------------------------------------\n\n");
+	fflush(stdout);
+	free_dlm(&dlm);
 
 	rewind(inp);
 	readdlm(inp,&dlm,' ',0,0,'#');
+	DLM_COUNT(dlm,readnum);
 	printf("------------- quotes = 0; comments = 0; -------------\n\n");
 	fprintf(out,"------------- quotes = 0; comments = 0; -------------\n\n");
 	writedlm(stdout,&dlm,' ');
 	writenum = writedlm(out,&dlm,' ');
-	fprintf(out,"%s %d\n","Total read-in words:",dlm.wordnum);
+	fprintf(out,"%s %d\n","Total read-in words:",readnum);
 	fprintf(out,"%s %d\n","Total write-out words:",writenum);
-	printf("%s %d\n","Total read-in words:",dlm.wordnum);
+	printf("%s %d\n","Total read-in words:",readnum);
 	printf("%s %d\n","Total write-out words:",writenum);
 	fprintf(out,"-----------------------------------------------------\n\n");
 	printf("-----------------------------------------------------\n\n");
+	fflush(stdout);
+	free_dlm(&dlm);
 
 	rewind(inp);
 	readdlm(inp,&dlm,' ',0,0,'#');
+	DLM_COUNT(dlm,readnum);
 	printf("--- quotes = 0; comments = 0; output delim = '\\t'; ---\n\n");
 	fprintf(out,"--- quotes = 0; comments = 0; output delim = '\\t'; ---\n\n");
 	writedlm(stdout,&dlm,'\t');
 	writenum = writedlm(out,&dlm,'\t');
-	fprintf(out,"%s %d\n","Total read-in words:",dlm.wordnum);
+	fprintf(out,"%s %d\n","Total read-in words:",readnum);
 	fprintf(out,"%s %d\n","Total write-out words:",writenum);
-	printf("%s %d\n","Total read-in words:",dlm.wordnum);
+	printf("%s %d\n","Total read-in words:",readnum);
 	printf("%s %d\n","Total write-out words:",writenum);
 	fprintf(out,"-----------------------------------------------------\n");
 	printf("-----------------------------------------------------\n");
+	fflush(stdout);
 	free_dlm(&dlm);
 
 	fclose(inp);
