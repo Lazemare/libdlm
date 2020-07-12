03510ff4fd8e1993dcc6c088a5bd8a4f5d8dc091
Lazemare <zjt033@gmail.com>
2020-07-13 00:57:39 +0800

    Restructured the whole routine

--- src/writedlm.c
+++ src/writedlm.c
@@ -4,24 +4,17 @@
 #include "delimitedfiles.h"
 
 /* Write delimited file. */
-int writedlm(FILE *fp, struct DLM *dlm, char delim)
+int writedlm(FILE *fp, dlm_t *dlm, char delim)
 {
 	int i,j;
-	int flag = 0;
 	int count = 0;
 
-	for (i=0; i < (*dlm).linenum; i++) {
-		flag = 0;
-		for (j=0; j < MAXWORDS; j++) {
-			if (strcmp((*dlm).list[i][j],"\0") > 0) {
-				fprintf(fp,"%s%c",(*dlm).list[i][j],delim);
-				flag = 1;
-				count++;
-			}
+	for (i = 0; i < dlm->linenum; i++) {
+		for (j = 0; j < dlm->wordnum[i]; j++) {
+			fprintf(fp,"%s%c",dlm->list[i][j],delim);
+			count++;
 		}
-		if (flag == 1) {
-			fprintf(fp,"\n\n");
-		}
+		fprintf(fp,"\n");
 	}
 	return count;
 }
